8e7fd37287d1d3f3013a7cdf2a3472ce2cc22788
sai phaye <samarth@sais-MacBook-Pro.local>
2023-05-11 14:10:06 +0700

    web-init

---
+++ script.js
@@ -0,0 +1,21 @@
+// Smooth scroll to section when link is clicked
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+/* Smooth scrolling */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'instant'
+        });
+    });
+});
